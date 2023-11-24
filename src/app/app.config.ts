a8988421df167d7b4287c1b7c04dbbf31b7fd7cb
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-11-24 20:48:19 +0100

    Redirect any path to root domain

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { RainDataService } from './rain-data.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [RainDataService]
+  providers: [RainDataService, provideRouter([])],
 };
