883865e0c596f1050cf1a7890d1aa1f78dffa436
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2023-11-25 13:49:47 +0100

    Install pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { RainDataService } from './rain-data.service';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
-  providers: [RainDataService, provideRouter([])],
+  providers: [
+    RainDataService,
+    provideRouter([]),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
 };
