f4377ba283ede3b459d30181fae59cd0054dbd78
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-01-03 15:22:11 +0100

    Install puppeteer

---
+++ e2e/tests/lluvias.e2e.ts
@@ -0,0 +1,9 @@
+import { setupBrowserHooks, getBrowserState } from './utils';
+
+describe('Lluvias app', function () {
+  setupBrowserHooks();
+
+  it('should TODO', async function () {
+    const { page } = getBrowserState();
+  });
+});
